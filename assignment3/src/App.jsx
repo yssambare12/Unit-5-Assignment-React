c9f4a79d099c3cd054526392858ab909338b3315
Yogesh  Sambare <bloggingscan@gmail.com>
2022-01-12 15:55:23 +0530

    updated

--- assignment3/src/App.jsx
+++ assignment3/src/App.jsx
@@ -3,12 +3,10 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  // let count=0;
   const [count,setCount]=useState(0);
   const changeCount=(value)=>{
-    // setCount((count<=0 && value===-1)?count:(count+value));//is working
     setCount((prev)=>{
-      return (prev<=0 && value===-1)?prev:(prev+value);//but if we don't want count previous value can be taken
+      return (prev<=0 && value===-1)?prev:(prev+value);
     });
     console.log("counter",count);
   };
@@ -19,10 +17,10 @@ function App() {
   return (
     <div className="App">
     <h3 className='coun'>Counter-{count}</h3>
-    <button className='mar' onClick={()=>changeCount(1)}>Increment by 1</button>
-    <button className='mar' onClick={()=>changeCount(-1)}>Decrement by 1</button>
+    <button className='mar' onClick={()=>changeCount(1)}>Increment 1</button>
+    <button className='mar' onClick={()=>changeCount(-1)}>Decrement 1</button>
     <button className='mar' onDoubleClick={()=>changeCount(count)}>Double</button>
-    <div>Counter is {count%2===0?"Even":"Odd"}</div>
+    <div>Counter value is {count%2===0?"Even":"Odd"}</div>
     </div>
   );
 }
