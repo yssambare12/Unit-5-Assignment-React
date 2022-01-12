6a8b43e5b46b76fe3cd1c0117877589f46c8ac2e
Yogesh  Sambare <bloggingscan@gmail.com>
2022-01-12 15:45:36 +0530

    updated

--- assignment3/src/App.jsx
+++ assignment3/src/App.jsx
@@ -1,37 +1,29 @@
-
-import { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // let count=0;
+  const [count,setCount]=useState(0);
+  const changeCount=(value)=>{
+    // setCount((count<=0 && value===-1)?count:(count+value));//is working
+    setCount((prev)=>{
+      return (prev<=0 && value===-1)?prev:(prev+value);//but if we don't want count previous value can be taken
+    });
+    console.log("counter",count);
+  };
+  if(count>20)
+  {
+    return <div className='coun'>Counter has reached its limit</div>
+  }
   return (
     <div className="App">
-    <Counter />
+    <h3 className='coun'>Counter-{count}</h3>
+    <button className='mar' onClick={()=>changeCount(1)}>Increment by 1</button>
+    <button className='mar' onClick={()=>changeCount(-1)}>Decrement by 1</button>
+    <button className='mar' onDoubleClick={()=>changeCount(count)}>Double</button>
+    <div>Counter is {count%2===0?"Even":"Odd"}</div>
     </div>
-  );
-}
-
-function Counter(){
-  const [count,setCount]=useState(0);
-
-  const increase=()=>{
-    setCount(count+1);
-  }
-  
-  const decrease=()=>{
-    setCount(count-1);
-  }
-
-  const double=()=>{
-    setCount(count*2);
-  }
-
-return( 
-  <div>
-    <h1>Counter:{count}</h1>
-    <button onClick={increase}>+</button>
-    <button onClick={decrease}>-</button>
-    <div><button onClick={double}>Double</button></div>
-  </div>  
   );
 }
 
